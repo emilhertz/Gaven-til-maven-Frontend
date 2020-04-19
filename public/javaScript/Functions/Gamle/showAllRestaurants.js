44beff91cbf6bbb9d850244691364701d745f72d
pvburleigh <pvburleigh@gmail.com>
2020-04-19 10:15:05 +0200

    showAllRestaurants bruger nu axios i stedet for fetch

--- public/javaScript/Functions/Gamle/showAllRestaurants.js
+++ public/javaScript/Functions/Gamle/showAllRestaurants.js
@@ -1,36 +1,18 @@
-showRestaurant = async() =>{
-    //Defining request options
-    const options = {
-    method: "GET"
-  };
+axios.get("http://localhost:4000/restaurant")
+    .then(response => {
+        let restaurants = response.data.restaurants.map((restaurant)=> {
+            return {
+                Navn: restaurant.name,
+                Beskrivelse: restaurant.description,
+                Addresse: `${restaurant.address.streetName} ${restaurant.address.streetNumber}, ${restaurant.address.zipCode} ${restaurant.address.city} ${restaurant.address.country}`,
+                Siddepladser: 10,
+                //same as when we used "this"
+                Book: restaurant
+            }
 
-  //Request that saves respond in variable
-  const res = await fetch('http://localhost:4000/restaurant', options)
-      //Procedure if no error
-      .then((response)=>{return response.json()})
-      //Procedure if fetch error (e.g. API not reachable)
-      .catch((e)=>{return console.log(e)});
-  if(res.errors) {
-    console.log(res.errors)
-  }
-
-  //return restaurants
-  let restaurants = res.restaurants;
-  let rdyForTableRestaurants = restaurants.map((restaurant)=> {
-     return {
-      Navn: restaurant.name,
-      Beskrivelse: restaurant.description,
-      Addresse: `${restaurant.address.streetName} ${restaurant.address.streetNumber}, ${restaurant.address.zipCode} ${restaurant.address.city} ${restaurant.address.country}`,
-      Siddepladser: 10,
-      //same as when we used "this"
-      Book: restaurant
-    }
-
-  });
-  //table is appended to the document
-  restaurantsDiv = document.getElementById("restaurants");
-  restaurantsDiv.appendChild(buildTable(rdyForTableRestaurants));
-};
-
-
-showRestaurant();
+        });
+        document.getElementById("restaurants").appendChild(buildTable(restaurants))
+    })
+    .catch(err => {
+        alert("Kunne ikke hente restauranter")
+    });
