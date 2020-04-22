e98c8aef0e39a6e2a1cb9f12a4d3d06807847f48
emilhertz <emhe19ae@student.cbs.dk>
2020-04-22 11:12:13 +0200

    deleteRestaurant.js

--- public/javaScript/Functions/Nye/deleteRestaurant.js
+++ public/javaScript/Functions/Nye/deleteRestaurant.js
@@ -1,12 +1,11 @@
 function deleteRestaurant(restaurantId) {
 
-    let body = {
-        restaurantId: restaurantId
-    }
+    //retrieves jwt from cookie
+    let token = Cookies.get('token');
 
-    console.log(Cookies.get("token"));
-
-    axios.delete('http://localhost:4000/restaurant', body, {headers:{"authorization":`${Cookies.get("token")}`}})
+    //Sends delete requst to the endpoint below, with jwt token in the header and the restaurant id in the body
+    axios.delete('http://localhost:4000/restaurant', {headers: {Authorization: token}, data: {restaurantId: restaurantId}})
+        //waits for response from API and executes one of the following
         .then((response) => {
             alert(response.data.message);
     })
