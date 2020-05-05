600e991dad34f62824759de07e05f73acb2b61c7
kristofferkristensen <krkr19ad@student.cbs.dk>
2020-05-05 13:30:44 +0200

    Kommentarer part 2

--- public/javaScript/Functions/databaseRequests/storeReservation.js
+++ public/javaScript/Functions/databaseRequests/storeReservation.js
@@ -1,10 +1,14 @@
 function storeReservation(reservation){
     //post request containing the Reservation-instance as body and a JWT authorization token in the header
+    //the parameter 'reservation' is defined in 'configure reservation'
     axios.post('http://localhost:4000/reservation', reservation, {headers:{"authorization":`${Cookies.get("token")}`}})
+        //if it gets a correct response it alerts the message "Reserveret!"
         .then(response => {
             alert(response.data.message);
+            //Refresh the html
             window.open("userReservations.html", "_self");
         })
+        //Else et alerts an error-message
         .catch(err => {
             alert("Kunne ikke reservere pladser")
         })
